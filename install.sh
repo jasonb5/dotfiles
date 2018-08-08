613ee96dc11068cff3afdfa5e9c7806f77f1614b
Jason Boutte <boutte.jason@gmail.com>
2018-08-08 10:52:54 -0700

    Adds cloning git repo to install script

--- install.sh
+++ install.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+
+error_log=$HOME/dotfile-err.log
+install_path=$HOME/devel
+dotfile_path=$install_path/dotfiles
+
+echo -n "Where would you like to install? (default: $install_path) "
+
+read candidate_path
+
+[[ ! -z "$candidate_path" ]] && install_path=$candidate_path
+
+echo "Installing in $install_path"
+
+pushd $install_path
+
+git clone https://github.com/jasonb5/dotfiles 2> $error_log
+
+popd
+
+pushd $dotfile_path
+
+popd
