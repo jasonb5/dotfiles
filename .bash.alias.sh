dda1d140d82722e58fc0dd151552f3ae64435d50
Jason Boutte <boutte.jason@gmail.com>
2018-08-08 14:27:00 -0700

    Adds some basic aliases

---
+++ .bash.alias.sh
@@ -0,0 +1,6 @@
+alias sb=". ${HOME}/.bashrc"
+
+alias eb="${EDITOR} ${HOME}/.bashrc"
+alias ev="${EDITOR} ${HOME}/.vimrc"
+alias ea="${EDITOR} ${HOME}/.bash.alias.sh"
+alias ef="${EDITOR} ${HOME}/.bash.function.sh"
