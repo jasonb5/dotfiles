3f220e5935d464d7aa8b2b353964b141210c019e
Jason Boutte <boutte.jason@gmail.com>
2018-08-10 21:38:57 +0000

    Resets install file

--- install.sh
+++ install.sh
@@ -1,32 +1,11 @@
 #! /bin/bash
 
 install_path="$HOME/devel"
-dotfile_path="$install_path/dotfiles"
 
-echo -n "Where would you like to install? (default: ${install_path}) "
+echo -n "Install path? (default ${install_path}): "
 
 read candidate_path
 
 [[ -n "${candidate_path}" ]] && install_path="${candidate_path}"
 
-echo "Installing in ${install_path}"
-
-pushd "${install_path}"
-
-git clone https://github.com/jasonb5/dotfiles
-
-popd
-
-pushd "${dotfile_path}"
-
-git submodule init
-
-git submodule update
-
-. .bash.function.sh
-
-link_files "$HOME" $(cat files.txt)
-
-popd
-
-[[ "$(is_installed vim)" == "0" ]] && vim +PluginInstall +qall
+echo "Installing into \"${install_path}\""
