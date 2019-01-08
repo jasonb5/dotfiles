c5070212e7fc4667e436dd0ec94786590f82048e
Jason Boutte <boutte.jason@gmail.com>
2019-01-08 07:21:51 -0800

    Fixes search multiple places for conda environment

--- .bash.init.sh
+++ .bash.init.sh
@@ -2,8 +2,8 @@ export DOTFILE_PATH=`cat ${HOME}/.dotfile_path`
 
 export TERM="xterm-256color"
 
-conda_path="/opt/conda/bin"
-conda_home_path="${HOME}/conda/bin"
+CONDA_PATH=("/opt/conda" "${HOME}/conda" "${HOME}/miniconda2")
 
-[[ -e "${conda_path}" ]] && prepend_path ${conda_path}
-[[ -e "${conda_home_path}" ]] && prepend_path ${conda_home_path}
+for x in ${CONDA_PATH[@]}; do
+  [[ -e "${x}/bin" ]] && prepend_path "${x}/bin"
+done
