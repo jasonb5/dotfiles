ee343e3ae19991807992d460003eb111ee6eba9a
Jason Boutte <boutte.jason@gmail.com>
2019-01-21 02:20:03 -0800

    Fixes PS1

--- .bash.function.sh
+++ .bash.function.sh
@@ -1,3 +1,15 @@
+function console_colors {
+  #Foreground
+  for clfg in {30..37} {90..97} 39 ; do
+    #Formatting
+    for attr in 0 1 2 4 5 7 ; do
+      #Print the result
+      echo -en "\e[${attr};49;${clfg}m ^[${attr};49;${clfg}m \e[0m"
+    done
+    echo #Newline
+  done
+}
+
 function kube_templates {
 cat << EOF > "$1.yaml"
 apiVersion: apps/v1
