2a191ef7c8e08879d7587241745a08e7d0cea200
Jason Boutte <boutte.jason@gmail.com>
2019-02-01 12:27:03 -0800

    Fixes PS1

--- .bash.init.sh
+++ .bash.init.sh
@@ -8,13 +8,11 @@ for x in ${CONDA_PATH[@]}; do
   [[ -e "${x}/bin" ]] && prepend_path "${x}/bin"
 done
 
-. /etc/os-release
+RESET="\[\e[0m\]"
+C1="\[\e[91m\]" # LIGHT RED
+C2="\[\e[94m\]" # LIGHT BLUE
+C3="\[\e[92m\]" # LIGHT GREEN
+C4="\[\e[95m\]" # LIGHT MAGENTA
+C5="\[\e[97m\]" # WHITE
 
-RESET="\e[0m"
-C1="\e[91m" # LIGHT RED
-C2="\e[94m" # LIGHT BLUE
-C3="\e[92m" # LIGHT GREEN
-C4="\e[95m" # LIGHT MAGENTA
-C5="\e[97m" # WHITE
-
-PS1="${C4}${NAME} (${VERSION_ID}):${RESET} ${C1}\u${RESSET}${C5}@${RESET}${C3}\h${RESET} ${C5}in${RESET} ${C2}\w${RESET}\n${C5}\$ "
+PS1="${C4}$(uname -s) ($(uname -r)):${RESET} ${C1}[\u]${RESET}${C5}@${RESET}${C3}\h${RESET} ${C5}in${RESET} ${C2}[\w]${RESET}\n${C5}$ ${RESET}"
