5ebe9dee8026e10446f9686709b6ad467d43e056
Jason Boutte <boutte.jason@gmail.com>
2020-02-25 01:15:48 -0800

    Adds function to create tiller rbac

--- .bash.function.sh
+++ .bash.function.sh
@@ -14,6 +14,29 @@ function get_field {
   tr -s " " | cut -d " " -f ${1}
 }
 
+function tiller_rbac {
+cat << EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: tiller
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: tiller
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: tiller
+    namespace: kube-system
+EOF
+}
+
 function dump_certs {
   if [[ $# < 1 ]]
   then
