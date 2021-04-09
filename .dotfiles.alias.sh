bf8c4b9847ce6d7baf7e7179c8911d6f330efc8c
Jason Boutte <boutte.jason@gmail.com>
2021-04-09 10:27:59 -0700

    Updates alias file

--- .dotfiles.alias.sh
+++ .dotfiles.alias.sh
@@ -1,9 +1,11 @@
 unalias -a
 
-alias ev="vim ~/.vimrc"
-alias ep="vim ~/.bash_profile"
-alias eb="vim ~/.dotfiles.bashrc"
-alias ef="vim ~/.dotfiles.functions.sh"
+alias ebp="vim ~/.bash_profile"
 alias ea="vim ~/.dotfiles.alias.sh"
-
-alias sb="source ~/.bash_profile"
+alias eb="vim ~/.dotfiles.bashrc"
+alias ebu="vim ~/.dotfiles.bashrc.user"
+alias ebd="vim ~/.dotfiles.bashrc.dynamic"
+alias ef="vim ~/.dotfiles.functions.sh"
+alias eg="vim ~/.gitconfig"
+alias et="vim ~/.tmux"
+alias ev="vim ~/.vimrc"
