f4381a1b52440951136feb65490cfe5a4f59d80a
Jason Boutte <boutte.jason@gmail.com>
2021-09-20 20:27:48 +0000

    Cleans out alias file

--- .dotfiles.alias.sh
+++ .dotfiles.alias.sh
@@ -1,18 +1,1 @@
 unalias -a
-
-alias ebp="vim ~/.bash_profile"
-alias ea="vim ~/.dotfiles.alias.sh"
-alias eb="vim ~/.dotfiles.bashrc"
-alias ebu="vim ~/.dotfiles.bashrc.user"
-alias ebd="vim ~/.dotfiles.bashrc.dynamic"
-alias ef="vim ~/.dotfiles.functions.sh"
-alias eg="vim ~/.gitconfig"
-alias et="vim ~/.tmux.conf"
-alias ev="vim ~/.vimrc"
-
-alias sb="source ~/.bashrc"
-
-alias k="kubectl"
-alias ks="kubectl -n kube-system"
-
-alias template_helm="cookiecutter ${DOTFILE_PATH}/cookiecutters/helmchart"
