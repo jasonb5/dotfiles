bc7c74bdcb384f3453eebb964baaafc6f12b2b91
Jason Boutte <boutte.jason@gmail.com>
2021-09-20 21:13:25 +0000

    Adds install/uninstall functions

--- .dotfiles.functions.sh
+++ .dotfiles.functions.sh
@@ -14,8 +14,43 @@ FILES=(
 .gitconfig
 )
 
+log() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
 function install_dotfiles {
+	for filename in "${FILES[@]}"; do
+		local dst_file="${HOME}/${filename}"
+		local src_file="${DOTFILE_PATH}/${filename}"
+
+		if [[ -e "${dst_file}" ]] && [[ ! -L "${dst_file}" ]]; then
+			log "Backing up ${dst_file}"
+
+			mv "${dst_file}" "${dst_file}.bak"
+		fi
+
+		if [[ ! -e "${dst_file}" ]]; then
+			log "Linking ${src_file} -> ${dst_file}"
+
+			ln -sf "${src_file}" "${dst_file}"
+		fi
+	done
 }
 
 function uninstall_dotfiles {
+	for filename in "${FILES[@]}"; do
+		local installed_file="${HOME}/${filename}"
+
+		if [[ -e "${installed_file}" ]] && [[ -L "${installed_file}" ]]; then
+			log "Unlinking ${installed_file}"
+
+			unlink "${installed_file}"
+		fi
+
+		if [[ -e "${installed_file}.bak" ]]; then
+			log "Restoring ${installed_file} backup"
+
+			mv "${installed_file}.bak" "${installed_file}"
+		fi
+	done
 }
