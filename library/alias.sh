6b0a737b272ba44bb84bc34cd6942d4301e526d0
Jason Boutte <boutte.jason@gmail.com>
2022-04-28 10:38:05 -0700

    Updates aliases and functions

--- library/alias.sh
+++ library/alias.sh
@@ -3,3 +3,8 @@ unalias -a
 alias sb="source ~/.bashrc"
 alias et="vim ~/.tmux.conf"
 alias ev="vim ~/.vimrc"
+
+alias ea="vim ${DOTFILE_PATH}/library/alias.sh"
+alias eb="vim ${DOTFILE_PATH}/library/bashrc.sh"
+alias ee="vim ${DOTFILE_PATH}/library/exports.sh"
+alias ef="vim ${DOTFILE_PATH}/library/functions.sh"
