24ae3e0002b67c719722883bc979d04ace089bd9
Jason Boutte <boutte.jason@gmail.com>
2022-07-18 13:05:23 -0700

    Updates tmux

--- library/functions.sh
+++ library/functions.sh
@@ -29,6 +29,18 @@ function dotfiles::install_mambaforge() {
     rm conda.sh
 }
 
+function tat {
+    name="$(basename `pwd` | sed -e 's/\.//g')"
+
+    if tmux ls 2>&1 | grep "${name}"; then
+        tmux attach -t "${name}"
+    elif [ -f .envrc ]; then
+        direnv exec / tmux new-session -s "${name}"
+    else
+        tmux new-session -s "${name}"
+    fi
+}
+
 #==============================
 # library functions
 #==============================
