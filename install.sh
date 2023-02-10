ba92c0300e611ac1b2a48d58de2726036305014d
Jason Boutte <boutte.jason@gmail.com>
2023-02-10 09:54:27 -0800

    Fixes checking out submodules

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ dotfile_path="${DOTFILE_PATH:-${HOME}/devel/dotfiles}"
 
 if [[ ! -e "${dotfile_path}" ]]; then
 	git clone https://github.com/jasonb5/dotfiles ${dotfile_path}
+
+	pushd "${dotfile_path}"
+
+	git submodule update --init
+
+	popd
 fi
 
 source "${dotfile_path}/library/functions.sh"
