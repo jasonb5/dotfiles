e31c7c0f45b36439c1dac63bb18d55ae7e9d0940
Jason Boutte <boutte.jason@gmail.com>
2023-02-10 09:54:49 -0800

    removes tmuxinator config copy

--- library/functions.sh
+++ library/functions.sh
@@ -12,7 +12,7 @@ export EDITOR=vim
 
 declare -a CONFIGS
 
-CONFIGS=( .gitconfig .tmux.conf .vim/coc-settings.json .vimrc .tmux/plugins/tpm .config/tmuxinator )
+CONFIGS=( .gitconfig .tmux.conf .vim/coc-settings.json .vimrc .tmux/plugins/tpm )
 DOTFILE_START="# >>>>>> DOTFILE_START >>>>>>"
 DOTFILE_STOP="# <<<<<< DOTFILE_STOP <<<<<<"
 VIM_PLUG_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
