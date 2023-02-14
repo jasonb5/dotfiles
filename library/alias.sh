45a6ce77fc61b64b6fa55cd00c9117e3863094f2
Jason Boutte <boutte.jason@gmail.com>
2023-02-14 14:31:17 -0800

    Fixes disabling new-mac on osx

--- library/alias.sh
+++ library/alias.sh
@@ -19,4 +19,6 @@ alias ubuntu="dotfiles::container_ubuntu"
 
 alias dev="dotfiles::dev"
 
-alias new-mac=" printf '%02x' $((0x$(od /dev/urandom -N1 -t x1 -An | tr -d ' ') & 0xFE | 0x02)); od /dev/urandom -N5 -t x1 -An | tr ' '  ':'"
+if [[ "$(uname)" == "Linux" ]]; then
+  alias new-mac=" printf '%02x' $((0x$(od /dev/urandom -N1 -t x1 -An | tr -d ' ') & 0xFE | 0x02)); od /dev/urandom -N5 -t x1 -An | tr ' '  ':'"
+fi
