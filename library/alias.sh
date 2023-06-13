a2280acdd63353d671a779ca71764b590a79f31c
Jason Boutte <boutte.jason@gmail.com>
2023-06-13 12:08:20 -0700

    Adds tmux alias for handling TERM variable

--- library/alias.sh
+++ library/alias.sh
@@ -13,4 +13,6 @@ alias ef="vim ${DOTFILE_PATH}/library/functions.sh"
 
 alias dev="dotfiles::dev"
 
+alias tmux="TERM=screen-256color-bce tmux -2"
+
 alias new-mac=" printf '%02x' $((0x$(od /dev/urandom -N1 -t x1 -An | tr -d ' ') & 0xFE | 0x02)); od /dev/urandom -N5 -t x1 -An | tr ' '  ':'"
