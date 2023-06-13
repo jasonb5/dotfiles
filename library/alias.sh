ae9c8fe5e8709bcecaca3430ad749093986caac9
Jason Boutte <boutte.jason@gmail.com>
2023-06-13 10:57:06 -0700

    Updates aliases

--- library/alias.sh
+++ library/alias.sh
@@ -11,12 +11,6 @@ alias eb="vim ${DOTFILE_PATH}/library/bashrc.sh"
 alias ee="vim ${DOTFILE_PATH}/library/exports.sh"
 alias ef="vim ${DOTFILE_PATH}/library/functions.sh"
 
-alias container="dotfiles::run_container"
-alias jupyter="dotfiles::container_jupyterlab"
-alias cime_e3sm="dotfiles::container_cime_e3sm"
-alias cime="cime_e3sm"
-alias ubuntu="dotfiles::container_ubuntu"
-
 alias dev="dotfiles::dev"
 
 alias new-mac=" printf '%02x' $((0x$(od /dev/urandom -N1 -t x1 -An | tr -d ' ') & 0xFE | 0x02)); od /dev/urandom -N5 -t x1 -An | tr ' '  ':'"
