979551d30b4fa3e685ae249283f7f306b65fca28
Jason Boutte <boutte.jason@gmail.com>
2024-05-20 11:25:04 -0700

    Updates library

--- library/functions.sh
+++ library/functions.sh
@@ -15,6 +15,27 @@ CONFIG_FILES=(
 )
 
 #==============================
+# Exports
+export GPG_TTY=$(tty)
+export EDITOR=vim
+#==============================
+
+CLEAR="\[\033[0m\]"
+PURPLE="\[\033[35m\]"
+CYAN="\[\033[36m\]"
+
+export PROMPT_COMMAND='EXIT="$?";
+  if [[ -n "${PRIVATE}" ]]; then
+    L1="${CONDA_PROMPT_MODIFIER:-}${PURPLE}user@$(dotfiles::utils::hostname): $(pwd)${CLEAR}";
+  else
+    L1="${CONDA_PROMPT_MODIFIER:-}${PURPLE}$(whoami)@$(dotfiles::utils::hostname): $(pwd)${CLEAR}";
+  fi
+
+  L2="${CYAN}${EXIT} $> ${CLEAR}";
+
+  PS1="${L1}\n${L2}";'
+
+#==============================
 # user functions
 #==============================
 
@@ -39,7 +60,6 @@ function dotfiles::user::usb_list() {
 
 function dotfiles::load() {
   source "${DOTFILE_PATH}/library/alias.sh"
-  source "${DOTFILE_PATH}/library/bashrc.sh"
   source "${DOTFILE_PATH}/library/functions.sh"
 }
 
@@ -98,6 +118,13 @@ function dotfiles::vimplug::uninstall() {
 # utility functions
 #==============================
 
+function dotfiles::utils::hostname() {
+  if [[ -z "${HOSTNAME}" ]]; then
+    hostnamectl hostname
+  else
+    echo "${HOSTNAME}"
+  fi
+}
 
 #==============================
 # symlink functions
