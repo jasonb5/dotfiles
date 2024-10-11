3421eb6c9a6e3b2f6e2878df0d720139c1f0edfa
Jason Boutte <boutte.jason@gmail.com>
2024-10-11 15:07:37 -0700

    Fixes e3sm path

--- machine/ganymede.sh
+++ machine/ganymede.sh
@@ -12,11 +12,11 @@ function _localai() {
 alias localai="_localai"
 
 function _cime_e3sm() {
-	dotfiles::container::run "-v ~/devel:/src -v ~/devel:/root/devel -e CIME_MODEL=e3sm -e CIME_MACHINE=docker -e SRC_PATH=/src -e SKIP_CIME_UPDATE=true -e SKIP_MODEL_SETUP=true -e DEBUG=true -w /src/E3SM/cime ghcr.io/esmci/cime:latest bash"
+	dotfiles::container::run "-v ~/devel:/src -v ~/devel:/root/devel -e CIME_MODEL=e3sm -e CIME_MACHINE=docker -e SRC_PATH=/src -e SKIP_CIME_UPDATE=true -e SKIP_MODEL_SETUP=true -e DEBUG=true -w /src/work/E3SM/cime ghcr.io/esmci/cime:latest bash"
 }
 alias cime="_cime_e3sm"
 
 function _cime_cesm() {
-	dotfiles::container::run "-v ~/devel:/src -v ~/devel:/root/devel -e CIME_MODEL=cesm -e CIME_MACHINE=docker -e SRC_PATH=/src -e SKIP_CIME_UPDATE=true -e SKIP_MODEL_SETUP=true -e DEBUG=true -w /src/CESM/cime ghcr.io/esmci/cime:latest bash"
+	dotfiles::container::run "-v ~/devel:/src -v ~/devel:/root/devel -e CIME_MODEL=cesm -e CIME_MACHINE=docker -e SRC_PATH=/src -e SKIP_CIME_UPDATE=true -e SKIP_MODEL_SETUP=true -e DEBUG=true -w /src/work/CESM/cime ghcr.io/esmci/cime:latest bash"
 }
 alias cime-cesm="_cime_cesm"
