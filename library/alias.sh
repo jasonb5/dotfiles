e53dc5a2032e6db030e2e95cdd9c0070be9a995f
Jason Boutte <boutte.jason@gmail.com>
2024-10-11 15:17:14 -0700

    Removes comments

--- library/alias.sh
+++ library/alias.sh
@@ -14,11 +14,6 @@ alias miniforge3="dotfiles::user::miniforge3"
 alias df-install="dotfiles::bashrc::append && source ${HOME}/.bashrc"
 alias df-uninstall="dotfiles::bashrc::remove && dotfiles::uninstall && source ${HOME}/.bashrc"
 
-# https://musigma.blog/2021/05/09/gpg-ssh-ed25519.html
-# https://benjamintoll.com/2023/09/06/on-creating-a-signing-subkey/
-# https://markentier.tech/posts/2021/02/github-with-multiple-profiles-gpg-ssh-keys/
-# https://insight.o-o.studio/article/setting-up-gpg.html
-
 alias scp-pass="scp -o PreferredAuthentications=password -o PubkeyAuthentication=no"
 alias ssh-pass="ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no"
 alias ssh-new="dotfiles::user::ssh::new"
