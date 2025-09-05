38ab1033091291891b39813321edfb40c2d89b12
Jason Boutte <boutte.jason@pm.me>
2025-09-05 11:11:32 -0700

    Adds hyprlock and initial arch config

---
+++ machines/arch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vim: set shiftwidth=2 tabstop=2 softtabstop=2 et:
+
+info "Running arch bootstrap"
+
+info "Updating system packages"
+
+sudo pacman -Syu
+
+info "Installing required packages"
+
+sudo pacman -Sy hyprlock
