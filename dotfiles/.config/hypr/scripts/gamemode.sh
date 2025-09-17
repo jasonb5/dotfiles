5a44d866ca5d91a47fe040259f49b4f356c0f963
Jason Boutte <boutte.jason@gmail.com>
2025-09-17 16:15:14 -0700

    Adds gamemode

---
+++ dotfiles/.config/hypr/scripts/gamemode.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+hyprgamemode="$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')"
+
+if [[ "${hyprgamemode}" -eq 1 ]]; then
+    hyprctl --batch "\
+        keyword animations:enabled 0; \
+        keyword animation borderangle,0; \
+        keyword decoration:shadow:enabled 0; \
+        keyword decoration:blur:enabled 0; \
+        keyword decoration:fullscreen_opacity 1; \
+        keyword general:gaps_in 0; \
+        keyword general:gaps_out 0; \
+        keyword general:border_size 1; \
+        keyword decoration:rounding 0"
+    hyprctl notify 1 5000 "rgb(40a02b)" "Gamemode [ON]"
+    exit 0
+else
+    hyprctl notify 1 5000 "rgb(d20f39)" "Gamemode [OFF]"
+    hyprctl reload
+    exit 0
+fi
+
+exit 1
