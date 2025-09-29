fcbd4399bd60ae0acb3808df01fc6910119c7dd1
Jason Boutte <boutte.jason@pm.me>
2025-09-29 15:38:31 -0700

    Updates main

---
+++ library/00_init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export GPG_TTY=$(tty)
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+gpg-connect-agent updatestartuptty /bye > /dev/null
--- library/20_source.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# vim: set shiftwidth=2 tabstop=2 softtabstop=2 et:
-
-safe_source "$(kernel_file)"
-safe_source "$(os_file)"
-safe_source "$(hostname_file)"
