6cb58b94281cc52930f48d319a0afe581fd59e17
Jason Boutte <boutte.jason@pm.me>
2025-09-30 14:06:15 -0700

    Updates nvim lsp config

--- dotfiles/.config/nvim/lua/blackhole/plugins/development.lua
+++ dotfiles/.config/nvim/lua/blackhole/plugins/development.lua
@@ -1,17 +1,31 @@
+local servers = {
+    pyright = {},
+    ruff = {},
+}
+
 return {
-    'kdheepak/lazygit.nvim',
-    lazy = true,
-    cmd = {
-        'LazyGit',
-        'LazyGitConfig',
-        'LazyGitCurrentFile',
-        'LazyGitFilter',
-        'LazyGitFilterCurrentFile',
+    {
+        'kdheepak/lazygit.nvim',
+        lazy = true,
+        cmd = {
+            'LazyGit',
+            'LazyGitConfig',
+            'LazyGitCurrentFile',
+            'LazyGitFilter',
+            'LazyGitFilterCurrentFile',
+        },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<CR>", desc = "opens lazygit" },
+        },
     },
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-    },
-    keys = {
-        { "<leader>lg", "<cmd>LazyGit<CR>", desc = "opens lazygit" },
+    {
+        'neomvim/nvim-lspconfig',
+        dependencies = {
+            { 'mason-org/mason.nvim', opts = {} },
+            { 'mason-org/mason-lspconfig.nvim', opts = { ensure_installed = vim.tbl_keys(servers) } },
+        },
     },
 }
