a7e2014a8067c06a3d77ccb3e26d6f3e3d8f38e0
Jason Boutte <boutte.jason@pm.me>
2025-10-09 01:46:04 -0700

    Updates creating/switching tmux sessions

--- library/linux-arch/functions.sh
+++ library/linux-arch/functions.sh
@@ -3,3 +3,18 @@
 function init_bash() {
     debug "Init bash" 
 }
+
+function tmuxs() {
+    local selected="$(find ~/devel -mindepth 2 -maxdepth 2 -type d | fzf)"
+    local name="${selected##$(dirname ${selected})/}"
+
+    if [[ -z "${TMUX}" ]]; then
+        tmux new-session -A -c "${selected}" -s "${name}"
+    else
+        if ! tmux has-session -t "${name}" 2>/dev/null; then
+            tmux new-session -c "${selected}" -s "${name}" -d
+        fi
+
+        tmux switch-client -t "${name}"
+    fi
+}
