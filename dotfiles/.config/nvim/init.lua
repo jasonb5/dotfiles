925cc19b2233ec379cae78bad5c543c5e939afee
Jason Boutte <boutte.jason@gmail.com>
2025-12-02 23:48:50 -0800

    Fixes treesitter loading

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -12,7 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('settings')
-require('autocmds')
 require('lsp')
 
 require('lazy').setup('plugins', {
--- dotfiles/.config/nvim/lua/autocmds.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = '*',
-    callback = function()
-        vim.treesitter.start()
-        vim.bo.indentexpr = 'v:lua.require("nvim-treesittter").indentexpr()'
-    end,
-})
