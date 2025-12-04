9ec7768fe4ce3d374f3d99ea899e1551dee2f3eb
Jason Boutte <boutte.jason@gmail.com>
2025-12-03 22:54:38 -0800

    Merges bootstrap functions

--- modules/linux-arch/functions.sh
+++ modules/linux-arch/functions.sh
@@ -1,5 +1,98 @@
 #!/usr/bin/env bash
 
+function setup_os() {
+    install_packages
+
+    setup_networkmanager
+    setup_tmux
+}
+
+function install_packages() {
+    if ! command_exists yay; then
+        local temp_dir
+        temp_dir="$(mktemp -d)"
+
+        git clone --filter blob:none https://aur.archlinux.org/yay.git "${temp_dir}"
+
+        pushd "${temp_dir}"
+
+        makepkg -i
+
+        info "Installed \"yay\""
+    fi
+
+    yay -Sy \
+        ttf-fira-code \
+        ttf-jetbrains-mono \
+        woff2-font-awesome \
+        nerd-fonts \
+        base-devel \
+        lua-language-server \
+        unzip \
+        git \
+        less \
+        lsb-release \
+        tmux \
+        neovim \
+        pacman-contrib \
+        playerctl \
+        brightnessctl \
+        hyprlock \
+        hypridle \
+        hyprpolkitagent \
+        wl-clipboard \
+        cliphist \
+        wtype \
+        sddm-astronaut-theme \
+        swww \
+        waybar \
+        rofi \
+        rofi-emoji \
+        rofi-nerdy \
+        fastfetch \
+        dunst \
+        fzf \
+        lazygit \
+        python-pywal16 \
+        networkmanager \
+        starship
+
+    if ! command_exists node; then
+        curl -o- https://fnm.vercel.app/install | bash
+
+        info "Installed \"fnm\" and \"node\""
+    fi
+
+    if ! command_exists gemini; then
+        npm install -g @google/gemini-cli
+
+        info "INstalled \"gemini\""
+    fi
+}
+
+function setup_networkmanager() {
+    if [[ "$(systemctl is-active systemd-networkd)" == "active" ]]; then
+        sudo systemctl stop systemd-networkd
+        sudo systemctl disable systemd-networkd
+    fi
+
+    if [[ "$(systemctl is-active iwd)" == "active" ]]; then
+        sudo systemctl stop iwd
+        sudo systemctl disable iwd
+    fi
+
+    sudo systemctl start NetworkManager
+    sudo systemctl enable NetworkManager
+}
+
+function setup_tmux() {
+    if command_exists tmux && [[ ! -e "${HOME}/.tmux/plugins/tpm"  ]]; then
+        info "Installing tmux tpm"
+
+        git clone --filter=blob:none https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+    fi
+}
+
 function init_bash() {
     export GPG_TTY=$(tty)
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
