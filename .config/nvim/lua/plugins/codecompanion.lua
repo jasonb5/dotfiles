5e0d4c3e5de206151048c40e51107e54881f8915
Jason Boutte <boutte.jason@pm.me>
2026-01-17 14:15:01 -0800

    chore(nvim): adds mcphub plugin and integrates with codecompanion

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -10,6 +10,21 @@ return {
             { '<leader>cc', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Toggle CodeCompanion chat' },
             { '<leader>ca', '<cmd>CodeCompanionChat Add<cr>', desc = 'Add to CodeCompanion chat', mode = 'x' },
         },
-        opts = {},
+        opts = {
+            extensions = {
+                mcphub = {
+                    callback = 'mcphub.extensions.codecompanion',
+                    opts = {
+                        make_tools = true,
+                        show_server_tools_in_chat = true,
+                        add_mcp_prefix_to_tool_names = false,
+                        show_result_in_chat = true,
+                        format_tool = nil,
+                        make_vars = true,
+                        make_slash_commands = true,
+                    },
+                },
+            },
+        },
     },
 }
