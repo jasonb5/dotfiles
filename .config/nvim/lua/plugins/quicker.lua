cfbbde5dc6494a4a2c96878f4a74e4f190f6cfbb
Jason Boutte <boutte.jason@gmail.com>
2026-01-24 15:19:39 -0800

    feat(nvim): update config to use filetype trigger and add new quickfix/context keymaps
    - Change plugin load trigger from "VeryLazy" event to "qf" filetype for efficiency.
    - Simplify keymap definitions for toggling quickfix and loclist.
    - Add keymaps for expanding and collapsing quickfix context ('>' to expand context, '<' to collapse).
    - Initialize plugin with explicit empty opts table.

--- .config/nvim/lua/plugins/quicker.lua
+++ .config/nvim/lua/plugins/quicker.lua
@@ -1,22 +1,15 @@
 return {
     {
         'stevearc/quicker.nvim',
-        event = 'VeryLazy',
+        ft = "qf",
+        ---@module "quicker"
+        ---@type quicker.SetupOptions
+        opts = {},
         keys = {
-            {
-                '<leader>q',
-                function()
-                    require('quicker').toggle()
-                end,
-                desc = 'Toggle quickfix',
-            },
-            {
-                '<leader>l',
-                function()
-                    require('quicker').toggle({ loclist = true })
-                end,
-                desc = 'Toggle quickfix with loclist',
-            },
+            { '<leader>q', function() require'quicker'.toggle() end, desc = 'Toggle quickfix' },
+            { '<leader>l', function() require'quicker'.toggle({ loclist = true }) end, desc = 'Toggle loclist' },
+            { '>', function() require'quicker'.expand({ before = 2, after = 2, add_to_existing = true }) end, desc = 'Expand quickfix context' },
+            { '<', function() require'quicker'.collapse() end, desc = 'Collapse quickfix context' },
         },
     },
 }
