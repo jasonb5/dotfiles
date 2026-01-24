4c5bad2e2ee7e1b69ab313bbb9730f7c678b38a9
Jason Boutte <boutte.jason@pm.me>
2026-01-24 02:58:57 -0800

    refactor(nvim): simplify LSP keymap setup and update Blink plugin config
    - Move LSP keymap bindings directly into the LspAttach autocmd callback, removing the separate on_attach function for clarity.
    - Unify signature help keymap to work in insert, normal, and visual modes.
    - Simplify vim.diagnostic.config setup.
    - Update blink.cmp plugin config: remove explicit keymap and completion options in favor of `keymap.preset = 'super-tab'`, and add version constraint.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,48 +1,38 @@
 local function on_attach(client, bufnr)
-    if client:supports_method 'textDocument/references' then
-        vim.keymap.set('n', '<leader>fr', '<cmd>FzfLua lsp_references<cr>')
-    end
-
-    if client:supports_method 'textDocument/definition' then
-        vim.keymap.set('n', '<leader>fd', '<cmd>FzfLua lsp_definitions<cr>')
-    end
-
-    if client:supports_method 'textDocument/implementation' then
-        vim.keymap.set('n', '<leader>fi', '<cmd>FzfLua lsp_implementations<cr>')
-    end
-
-    if client:supports_method 'textDocument/documentSymbol' then
-        vim.keymap.set('n', '<leader>fs', '<cmd>FzfLua lsp_document_symbols<cr>')
-    end
-
-    if client:supports_method 'textDocument/codeAction' then
-        vim.keymap.set('n', '<leader>ca', '<cmd>FzfLua lsp_code_actions<cr>')
-    end
-
-    if client:supports_method 'textDocument/signatureHelp' then
-        vim.keymap.set('i', '<C-k>', function()
-            vim.lsp.buf.signature_help()
-        end)
-    end
 end
 
-vim.diagnostic.config {
-    virtual_text = {
-        prefix = '',
-        spacing = 2,
-    },
-    float = {
-        source = 'if_many',
-    },
-    signs = false,
-}
+vim.diagnostic.config {}
 
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         local buf = args.buf
 
-        on_attach(client, buf)
+        if client:supports_method 'textDocument/references' then
+            vim.keymap.set('n', '<leader>fr', '<cmd>FzfLua lsp_references<cr>')
+        end
+
+        if client:supports_method 'textDocument/definition' then
+            vim.keymap.set('n', '<leader>fd', '<cmd>FzfLua lsp_definitions<cr>')
+        end
+
+        if client:supports_method 'textDocument/implementation' then
+            vim.keymap.set('n', '<leader>fi', '<cmd>FzfLua lsp_implementations<cr>')
+        end
+
+        if client:supports_method 'textDocument/documentSymbol' then
+            vim.keymap.set('n', '<leader>fs', '<cmd>FzfLua lsp_document_symbols<cr>')
+        end
+
+        if client:supports_method 'textDocument/codeAction' then
+            vim.keymap.set('n', '<leader>ca', '<cmd>FzfLua lsp_code_actions<cr>')
+        end
+
+        if client:supports_method 'textDocument/signatureHelp' then
+            vim.keymap.set({'i', 'n', 'x'}, '<C-k>', function()
+                vim.lsp.buf.signature_help()
+            end)
+        end
     end,
 })
 
--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,32 +1,13 @@
 return {
     {
         'saghen/blink.cmp',
-        build = 'cargo +nightly build --release',
+        version = '1.*',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
         opts = {
-            keymap = {
-                ['<CR>'] = { 'accept', 'fallback' },
-                ['<C-\\>'] = { 'hide', 'fallback' },
-                ['<C-n>'] = { 'select_next', 'show' },
-                ['<Tab>'] = { 'select_next', 'fallback' },
-                ['<C-p>'] = { 'select_prev' },
-                ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-                ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-            },
-        },
-        completion = {
-            list = {
-                selection = { preselect = false, auto_insert = true },
-                max_items = 10,
-            },
-            documentation = { auto_show = true },
-            menu = {
-                scrollbar = false,
-            }
-        },
-        keys = {
-            { '<leader>bp', '<cmd>BufferLinePick<cr>', desc = 'Select buffer to open' },
-            { '<leader>bc', '<cmd>BufferLinePickClose<cr>', desc = 'Select buffer to close' },
-            { '<leader>bo', '<cmd>BufferLineCloseOthers<cr>', desc = 'Close others' },
-        },
+            keymap = { preset = 'super-tab' },
+            completion = { documentation = { auto_show = true } },
+        }
     },
 }
