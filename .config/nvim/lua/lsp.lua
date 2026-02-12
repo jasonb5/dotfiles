e27ad47c53965480fdec5cb3f4d71b6d3f766c5a
Jason Boutte <boutte.jason@pm.me>
2026-02-12 01:08:02 -0800

    fix(lsp): enable virtual_text for diagnostics by default
    Adds vim.diagnostic.config with virtual_text enabled in the LSP setup autocmd to display inline diagnostics on buffer open. This improves error and warning visibility in the code.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -48,6 +48,10 @@ vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
     callback = function()
         vim.lsp.config('*', { capabilities = require'blink.cmp'.get_lsp_capabilities(nil, true) })
 
+        vim.diagnostic.config({
+            virtual_text = true
+        })
+
         local servers = vim.iter(vim.api.nvim_get_runtime_file('lsp/*.lua', true))
         :map(function(file)
             return vim.fn.fnamemodify(file, ':t:r')
