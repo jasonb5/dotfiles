3de684a9b578d8f1c6acb2fec7c4c2d44649b478
Jason Boutte <boutte.jason@pm.me>
2026-02-21 13:42:29 -0800

    style(bufferline.lua): reformat indentation to use tabs instead of spaces
    Switched indentation to tabs for consistency in bufferline.nvim config file.

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,11 +1,11 @@
 return {
-    {
-        'akinsho/bufferline.nvim',
-        opts = {
-            options = {
-                show_close_icon = false,
-                show_buffer_close_icons = false,
-            },
-        },
-    },
+	{
+		"akinsho/bufferline.nvim",
+		opts = {
+			options = {
+				show_close_icon = false,
+				show_buffer_close_icons = false,
+			},
+		},
+	},
 }
