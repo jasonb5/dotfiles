6de9785d0522f42ab7224324928fefb60f3b92da
Jason Boutte <boutte.jason@gmail.com>
2026-02-21 13:27:59 -0800

    chore(nvim): reformat flash.nvim plugin config with consistent indentation and style

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,10 +1,24 @@
 return {
-    {
-        'folke/flash.nvim',
-        event = 'VeryLazy',
-        keys = {
-            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        },
-    },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+		},
+	},
 }
