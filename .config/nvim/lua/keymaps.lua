4e6131dae1edc6f8f52b9f86c420494fdea07697
Jason Boutte <boutte.jason@pm.me>
2026-02-21 02:13:29 -0800

    refactor(keymaps): streamline and reorganize keymaps
    - Remove custom floating terminal and tab navigation mappings
    - Add new split window mappings (<leader>- and <leader>\)
    - Add <leader>wc to close the current window
    - Add <leader>qq to quit all without saving
    - Move and clarify window navigation mappings
    - Add <leader>/ to clear search highlighting
    - Remove redundant or unused mappings
    
    Improves maintainability and focuses keymaps on core window/buffer navigation.

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,61 +1,29 @@
--- Indent while in visual mode.
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
+vim.g.mapleader = ' '
 
--- Open package manager.
-vim.keymap.set('n', '<leader>L', '<cmd>Lazy<cr>', { desc = 'Open Lazy' })
+-- Search
+vim.keymap.set('n', '<leader>/', '<cmd>noh<cr>', { desc = 'Clear highlighting' })
 
--- Switch between windows
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window', remap = true })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to bottom window', remap = true })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to top window', remap = true })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window', remap = true })
+
+-- Quit
+vim.keymap.set('n', '<leader>qq', '<cmd>qall!<cr>', { desc = 'Quit all without saving' })
+
 
 -- Buffer navigation
 vim.keymap.set('n', 'H', '<cmd>bprevious<cr>', { desc = 'Go to previous buffer' })
 vim.keymap.set('n', 'L', '<cmd>bnext<cr>', { desc = 'Go to next buffer' })
 
--- Open terminal in floating window
-vim.keymap.set('n', '<leader>ft', function()
-  -- Improved floating terminal
-  local buf = vim.api.nvim_create_buf(false, true)
-  local width = math.floor(vim.o.columns * 0.8)
-  local height = math.floor(vim.o.lines * 0.8)
-  local win = vim.api.nvim_open_win(buf, true, {
-    relative = 'editor',
-    width = width,
-    height = height,
-    row = math.floor((vim.o.lines - height) / 2),
-    col = math.floor((vim.o.columns - width) / 2),
-    border = 'rounded',
-  })
 
-  -- Set buffer/window options for terminal UX
-  vim.api.nvim_set_option_value('number', false, { win = win })
-  vim.api.nvim_set_option_value('relativenumber', false, { win = win })
-  vim.api.nvim_set_option_value('signcolumn', 'no', { win = win })
+-- Split windows
+vim.keymap.set('n', '<leader>-', '<C-w>s', { desc = 'Split window horizontally' })
+vim.keymap.set('n', '<leader>\\', '<C-w>v', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>wc', '<C-w>q', { desc = 'Close current window' })
 
-  -- Open terminal attached to the buffer & win
-  vim.fn.termopen(vim.o.shell, {
-    on_exit = function()
-      -- Automatically close the window and buffer
-      if vim.api.nvim_win_is_valid(win) then
-        vim.api.nvim_win_close(win, true)
-      end
-      if vim.api.nvim_buf_is_valid(buf) then
-        vim.api.nvim_buf_delete(buf, { force = true })
-      end
-    end,
-  })
-  vim.cmd('startinsert!')
 
-  -- Map <Esc> to exit terminal and close
-  vim.keymap.set('t', '<Esc>', function()
-    vim.api.nvim_win_close(win, true)
-  end, { buffer = buf })
-end, { desc = 'Floating Terminal' })
+-- Window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to window below' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to window above' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
 
--- Tab navigation
-vim.keymap.set('n', '<leader>tc', '<cmd>tabclose<cr>', { desc = 'Close tab' })
-vim.keymap.set('n', '<leader>tn', '<cmd>tab split<cr>', { desc = 'New tab' })
-vim.keymap.set('n', '<leader>to', '<cmd>tabonly<cr>', { desc = 'Close other tabs' })
+
+
