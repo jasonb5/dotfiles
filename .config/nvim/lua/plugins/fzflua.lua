3c4a4f2f18fba4ba07b5847e98c000bdefabacbb
Jason Boutte <boutte.jason@pm.me>
2026-02-21 13:37:42 -0800

    refactor(fzflua): simplify key mappings to only include 'Find files'
    Reduce the available key mappings for fzf-lua to just <leader>ff for finding files. Removed buffer and live grep mappings for a more minimal setup.

--- .config/nvim/lua/plugins/fzflua.lua
+++ .config/nvim/lua/plugins/fzflua.lua
@@ -1,11 +1,15 @@
 return {
-    {
-        'ibhagwan/fzf-lua',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        keys = {
-            {'<leader>ff', function() require'fzf-lua'.files() end, desc = 'Find files'},
-            {'<leader>fb', function() require'fzf-lua'.buffers() end, desc = 'Find buffer'},
-            {'<leader>sf', function() require'fzf-lua'.live_grep() end, desc = 'Grep current project'},
-        }
-    },
+	{
+		"ibhagwan/fzf-lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					require("fzf-lua").files()
+				end,
+				desc = "Find files",
+			},
+		},
+	},
 }
