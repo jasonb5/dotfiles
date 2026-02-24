5b59c01e7cee94d4808d3fab2d2a5b177855d6f0
Jason Boutte <boutte.jason@pm.me>
2026-02-23 23:09:04 -0800

    style(nvim): reformat treesitter plugin config for consistency

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,39 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        lazy = false,
-        branch = 'main',
-        build = ':TSUpdate',
-        config = function(self, opts)
-            require'nvim-treesitter'.setup {}
-            require'nvim-treesitter'.install(opts.parsers)
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
+		build = ":TSUpdate",
+		opts = {
+			parsers = {
+				"bash",
+				"gitcommit",
+				"jinja",
+				"jinja_inline",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"regex",
+				"rust",
+				"toml",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter").setup({})
+			require("nvim-treesitter").install(opts.parsers)
 
-            vim.api.nvim_create_autocmd('FileType', {
-                pattern = opts.parsers,
-                callback = function()
-                    vim.treesitter.start()
-                    vim.bo.indentexpr = 'v:lua.require("nvim-treesittter").indentexpr()'
-                end,
-            })
-        end,
-        opts = {
-            parsers = {
-                'bash',
-                'gitcommit',
-                'jinja',
-                'jinja_inline',
-                'json',
-                'lua',
-                'markdown',
-                'markdown_inline',
-                'python',
-                'regex',
-                'rust',
-                'toml',
-                'vim',
-                'vimdoc',
-                'yaml',
-            },
-        },
-    },
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = opts.parsers,
+				callback = function()
+					vim.treesitter.start()
+					vim.bo.indentexpr = 'v:lua.require("nvim-treesittter").indentexpr()'
+				end,
+			})
+		end,
+	},
 }
