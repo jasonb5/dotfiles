c4b11d673c1d881f03d4467089c8c85497584602
Jason Boutte <boutte.jason@gmail.com>
2026-03-07 21:15:20 -0800

    feat(nvim): add snippet support

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -4,11 +4,25 @@ return {
 		version = "1.*",
 		dependencies = {
 			"Kaiser-Yang/blink-cmp-avante",
+      {
+        'L3MON4D3/LuaSnip',
+        version = 'v2.*',
+        build = "make install_jsregexp",
+        dependencies = {
+          "rafamadriz/friendly-snippets",
+        },
+        config = function()
+          require("luasnip").setup({})
+
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+      },
 		},
 		opts = {
 			keymap = {
 				preset = "super-tab",
 			},
+      snippets = { preset = "luasnip" },
 			sources = {
 				default = { "lsp", "avante", "path", "snippets", "buffer" },
 				providers = {
