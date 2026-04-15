385e7033f15996a9fa109c59bf015ae53e711f18
Jason Boutte <boutte.jason@pm.me>
2026-04-14 23:49:23 -0700

    feat(arch): add common package bootstrap

---
+++ bootstrap/distro/arch/05-common.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source "${DOTFILES_ROOT:?}/lib/common.sh"
+source "${DOTFILES_ROOT:?}/lib/log.sh"
+
+dotfiles_log_info "installing common Arch packages"
+sudo pacman -S --needed --noconfirm neovim tree
+
+dotfiles_log_info "common Arch packages installed"
