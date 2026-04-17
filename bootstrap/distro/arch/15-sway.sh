44ed24b2f6a2520becabd52b2fd18d8a5f350505
Jason Boutte <boutte.jason@pm.me>
2026-04-16 21:39:03 -0700

    feat(ironbar): add tuned profile switcher

--- bootstrap/distro/arch/15-sway.sh
+++ bootstrap/distro/arch/15-sway.sh
@@ -6,6 +6,7 @@ source "${DOTFILES_ROOT:?}/lib/common.sh"
 source "${DOTFILES_ROOT:?}/lib/log.sh"
 
 dotfiles_log_info "installing sway and runtime tools"
-sudo pacman -S --needed --noconfirm sway swaybg swayidle swaylock fuzzel grim slurp wl-clipboard kitty ironbar swaync ttf-iosevka-nerd xorg-xwayland xdg-desktop-portal-wlr upower
+sudo pacman -S --needed --noconfirm sway swaybg swayidle swaylock fuzzel grim slurp wl-clipboard kitty ironbar swaync ttf-iosevka-nerd xorg-xwayland xdg-desktop-portal-wlr upower tuned polkit-gnome
+sudo systemctl enable --now tuned
 
 dotfiles_log_info "sway installed"
