63e4af55f8833fac295aa5a06f1fc14e05a9520e
Jason Boutte <boutte.jason@pm.me>
2026-04-18 00:32:00 -0700

    feat(nvim): add buffer navigation shortcuts

--- config/distro/arch/.config/nvim/lua/config/keymaps.lua
+++ config/distro/arch/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,5 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>sv", "<C-w>v")
 keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { silent = true, desc = "Previous buffer" })
+keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { silent = true, desc = "Next buffer" })
