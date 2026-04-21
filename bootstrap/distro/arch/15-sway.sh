bf351ea9cbb4201ddd5abc285eda531bd4ad3481
Jason Boutte <boutte.jason@pm.me>
2026-04-21 15:38:38 -0700

    fix(dotfiles): add arch labwc launch helpers

--- bootstrap/distro/arch/15-sway.sh
+++ bootstrap/distro/arch/15-sway.sh
@@ -6,7 +6,7 @@ source "${DOTFILES_ROOT:?}/lib/common.sh"
 source "${DOTFILES_ROOT:?}/lib/log.sh"
 
 dotfiles_log_info "installing sway and runtime tools"
-sudo pacman -S --needed --noconfirm sway swaybg swayidle swaylock fuzzel grim slurp wl-clipboard kitty ironbar swaync ttf-iosevka-nerd xorg-xwayland xdg-desktop-portal-wlr upower tuned polkit-gnome
+sudo pacman -S --needed --noconfirm sway swaybg swayidle swaylock fuzzel grim slurp wl-clipboard kitty firefox labwc ironbar swaync obs-studio ttf-iosevka-nerd xorg-xwayland xdg-desktop-portal-wlr xdg-desktop-portal-gtk upower tuned polkit-gnome
 sudo systemctl enable --now tuned
 
 dotfiles_log_info "sway installed"
--- shell/common/00-aliases.sh
+++ shell/common/00-aliases.sh
@@ -1,6 +1,8 @@
 unalias -a 2>/dev/null || true
 
 alias cddot='cd "$DOTFILES_ROOT"'
+alias webex-kitty='${DOTFILES_ROOT:?}/scripts/distro/arch/labwc-run kitty'
+alias webex-firefox='${DOTFILES_ROOT:?}/scripts/distro/arch/labwc-run env MOZ_ENABLE_WAYLAND=1 firefox --no-remote -P webex'
 
 dotfile() {
   "${DOTFILES_ROOT:?}/dotfiles" "$@"
