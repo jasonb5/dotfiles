fc5cc4c4aeef815260416a5dec93368d3dbf77a4
Jason Boutte <boutte.jason@pm.me>
2026-05-13 15:20:46 -0700

    feat(bootstrap): include chromium in Arch sway package set

--- bootstrap/distro/arch/15-sway.sh
+++ bootstrap/distro/arch/15-sway.sh
@@ -6,7 +6,11 @@ source "${DOTFILES_ROOT:?}/lib/common.sh"
 source "${DOTFILES_ROOT:?}/lib/log.sh"
 
 dotfiles_log_info "installing sway and runtime tools"
-sudo pacman -S --needed --noconfirm sway swaybg swayidle swaylock fuzzel grim slurp wl-clipboard kitty firefox labwc ironbar swaync obs-studio ttf-iosevka-nerd xorg-xwayland xdg-desktop-portal-wlr xdg-desktop-portal-gtk upower tuned polkit-gnome
+sudo pacman -S --needed --noconfirm \
+  sway swaybg swayidle swaylock fuzzel grim slurp wl-clipboard \
+  kitty chromium firefox labwc ironbar swaync obs-studio ttf-iosevka-nerd xorg-xwayland \
+  xdg-desktop-portal-wlr xdg-desktop-portal-gtk \
+  upower tuned polkit-gnome
 sudo systemctl enable --now tuned
 
 dotfiles_log_info "sway installed"
